84ef60b08214a522c6a608917f9061c9d4ed56f7
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-12-13 14:44:58 -0500

    adding starting point of past react app code

---
+++ src/EditBookmark/EditBookmark.js
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BookmarksContext from '../BookmarksContext';
+import config from '../config'
+import './EditBookmark.css';
+
+const Required = () => (
+  <span className='EditBookmark__required'>*</span>
+)
+
+class EditBookmark extends Component {
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func,
+    }).isRequired,
+  };
+
+  static contextType = BookmarksContext;
+
+  state = {
+    error: null,
+    title: '',
+    url: '',
+    description: '',
+    rating: 1
+  };
+
+  componentDidMount() {
+    console.log('happens when component has mounted')
+    if (this.context.bookmarks.length === 0) {
+      this.props.history.push('/');
+      return;
+    }
+    const bookmark = this.context.bookmarks.find(b => b.id === Number(this.props.match.params.bookmarkId))
+
+    this.setState({
+      title: bookmark.title,
+      url: bookmark.url,
+      description: bookmark.description,
+      rating: bookmark.rating
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    // get the form fields from the event
+    const { title, url, description, rating } = e.target
+    const bookmark = {
+      title: title.value,
+      url: url.value,
+      description: description.value,
+      rating: Number(rating.value),
+    }
+    this.setState({ error: null })
+    fetch(config.API_ENDPOINT, {
+      method: 'PATCH',
+      body: JSON.stringify(bookmark),
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${config.API_KEY}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          return res.json().then(error => Promise.reject(error))
+        }
+        return res.json()
+      })
+      .then(data => {
+        title.value = ''
+        url.value = ''
+        description.value = ''
+        rating.value = ''
+        this.context.EditBookmark(data)
+        this.props.history.push('/')
+      })
+      .catch(error => {
+        console.error(error)
+        this.setState({ error })
+      })
+  }
+
+  handleClickCancel = () => {
+    this.props.history.push('/')
+  };
+
+  render() {
+    console.log(this.context)
+    const { error } = this.state
+    return (
+      <section className='EditBookmark'>
+        <h2>Edit bookmark</h2>
+        <form
+          className='EditBookmark__form'
+          onSubmit={this.handleSubmit}
+        >
+          <div className='EditBookmark__error' role='alert'>
+            {error && <p>{error.message}</p>}
+          </div>
+          <div>
+            <label htmlFor='title'>
+              Title
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='text'
+              name='title'
+              id='title'
+              placeholder='Great website!'
+              value={this.state.title}
+              onChange={e => this.setState({ title: e.target.value })}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='url'>
+              URL
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='url'
+              name='url'
+              id='url'
+              placeholder='https://www.great-website.com/'
+              value={this.state.url}
+              onChange={e => this.setState({ title: e.target.value })}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='description'>
+              Description
+            </label>
+            <textarea
+              name='description'
+              id='description'
+              value={this.state.description}
+              onChange={e => this.setState({ title: e.target.value })}
+            />
+          </div>
+          <div>
+            <label htmlFor='rating'>
+              Rating
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='number'
+              name='rating'
+              id='rating'
+              min='1'
+              max='5'
+              value={this.state.rating}
+              onChange={e => this.setState({ title: e.target.value })}
+              required
+            />
+          </div>
+          <div className='EditBookmark__buttons'>
+            <button type='button' onClick={this.handleClickCancel}>
+              Cancel
+            </button>
+            {' '}
+            <button type='submit'>
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default EditBookmark;
